ad4135d8451d713b28d21f30fdfb28e9c7a19a74
Brian <karabinchak.brian@gmail.com>
2017-09-19 22:58:00 -0400

    Changed head of menus, hoverable menu

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
+	//Materialize intiializiations
 	$('.parallax').parallax();
     $('.collapsible').collapsible();
 	$('ul.tabs').tabs();
+
+	//Custom
+	$('tbody tr').hover(
+		//handler in
+		function(){
+			$(this).addClass('z-depth-4 blue darken-3');
+		},
+		//handler out
+		function(){
+			$(this).removeClass('z-depth-4 blue darken-3');
+		});
 });
